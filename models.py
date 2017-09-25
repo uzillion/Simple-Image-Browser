4e7fc6958e33a9bd33a6aba3e7b7d3cd887c9847
Uzair Inamdar (uzillion) <uzair_inamdar@hotmail.com>
2017-09-25 00:27:31 -0700

    Add persisting tags

--- models.py
+++ models.py
@@ -2,13 +2,17 @@ import os
 from PyQt5.QtCore import QUrl
 from PyQt5.QtMultimedia import QSoundEffect
 class Models:
+
+#======================== Images =======================
     images = []
-    select_sound = QSoundEffect()
-    next_set_sound = QSoundEffect()
     for file_ in os.listdir("./data"):
         x=4
         if file_.endswith((".jpg", ".jpeg", ".png")):
             images.append(file_)
+
+#======================== Sounds =======================
+    select_sound = QSoundEffect()
+    next_set_sound = QSoundEffect()
     select_sound.setSource(QUrl.fromLocalFile(os.path.join('sounds','click.wav')))
     next_set_sound.setSource(QUrl.fromLocalFile(os.path.join('sounds','camera-roll.wav')))
     select_sound.setLoopCount(0)
--- view.py
+++ view.py
@@ -1,7 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QPushButton
 from PyQt5.QtGui import QPixmap
 from models import *
 from PyQt5.QtCore import *
+import json
 class View(QWidget):
 
     def __init__(self, W):
@@ -10,19 +11,41 @@ class View(QWidget):
         self.image_index = 0        # Keeps track of the first image in image list to be displayed in the thumbnail bar
         self.mode = 0         # Determines and Tracks current mode of view (1 - Full Screen, 0 - Thumbnail)
         self.selected_index = 0     # Keeps track of the current selected index in the thumbnail bar
+        self.loadTags()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Image Browser")
         self.setStyleSheet("background-color: #324d7a")
-        self.move(250, 250)
         self.H = (self.W/4)*3
+        self.move(250, 250)
         self.setFixedSize(self.W, self.H)
         self.layout = QHBoxLayout(self)     # Creates a Horizontal Layout
         self.layout.setContentsMargins(20, 0, 20, 0)    # Removes margins around stored object in the layout
         self.layout.setSpacing(0)           # Removes spaces between layout objects
+        self.layout.setAlignment(Qt.AlignCenter)    # Pushes image to the top of the window
+        self.textBox = QLineEdit(self)
+        self.textBox.resize(self.W/2,self.H/10)
+        self.textBox.move(20, 750)
+        self.addButton = QPushButton('Add Tag', self)
+        self.addButton.setStyleSheet("background-color: #F5F5F5")
+        self.addButton.move(self.W/1.9,750)
+        self.addButton.resize(self.W/6, self.H/10)
+        self.saveButton = QPushButton('Save Tags', self)
+        self.saveButton.setStyleSheet("background-color: #F5F5F5")
+        self.saveButton.move(self.W/1.42,750)
+        self.saveButton.resize(self.W/6, self.H/10)
+        self.addButton.clicked.connect(self.addTag)
+        self.saveButton.clicked.connect(self.saveTags)
+        self.tagList = QVBoxLayout()
+        self.tagList.setSpacing(10)
+        self.tagList.setContentsMargins(40, 0, 20, 0)
+        self.textBox.hide()
+        self.addButton.hide()
+        self.saveButton.hide()
         self.thumbnail_bar()
         self.select(0) # Selects first image-box in layou
+        self.setFocus()
         self.show()
 
     def thumbnail_bar(self):     # Creates a List of QLabels and places them in a horizontal Layout
@@ -37,10 +60,24 @@ class View(QWidget):
             self.layout.addWidget(labels[i])        # Add label into layout container
             self.clickable(self.getWidget(i)).connect(self.indexOfLabel)     # Connects the click even to the indexOfLabel function
 
+        self.layout.addLayout(self.tagList)
         # Properties of full screen view label:
         self.getWidget(5).setStyleSheet('border: 20px solid red')
-        self.getWidget(5).setMinimumSize(self.W, self.H)
+        self.getWidget(5).setFixedSize(self.W/1.33, self.H/1.28)
         self.getWidget(5).hide()
+
+    def drawTags(self):
+        image_key = str((self.image_index+self.selected_index)%len(Models.images))
+        if image_key in self.tags:
+            for i in range(0, len(self.getTags())):
+                self.getTags().itemAt(0).widget().setParent(None)
+            for tag in self.tags[image_key]:
+                self.getTags().addWidget(QPushButton(tag, self))
+                self.getTags().itemAt(len(self.getTags())-1).widget().setStyleSheet("background-color: #f5f5f5")
+        else:
+            for i in range(0, len(self.getTags())):
+                item = self.getTags().itemAt(0).widget().setParent(None)
+
 
     def enlarge(self, index):    # Makes necessary changes to change to Full Screen (Window Fill) Mode
             self.mode = 1
@@ -49,8 +86,15 @@ class View(QWidget):
             for i in range(0, 5):
                 self.getWidget(i).hide()
             self.getWidget(5).show()    # Shows label that displays in full screen
-            self.getWidget(5).setPixmap(pixContainer.scaled(self.W-40, self.H-40, Qt.KeepAspectRatio))   # Sets image in selected label to full screen label
-            self.layout.setContentsMargins(0, 0, 0, 500)    # Pushes image to the top of the window
+            self.getWidget(5).setPixmap(pixContainer.scaled((self.W/1.33)-40, (self.H/1.28)-40, Qt.KeepAspectRatio))   # Sets image in selected label to full screen label
+            self.layout.setContentsMargins(0, 0, 0, 0)    # Pushes image to the top of the window
+            self.layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)    # Pushes image to the top of the window
+            self.getTags().setAlignment(Qt.AlignRight)
+            self.textBox.show()
+            self.addButton.show()
+            self.saveButton.show()
+            self.drawTags()
+
 
     def circularTraverse(self, steps, direction):    # Responsible for Circular Traversing the image list in the given direction
         if direction == "left":
@@ -67,7 +111,7 @@ class View(QWidget):
         if self.selected_index != 0:         # Shift select box till first image
             if self.mode == 1:
                 pixContainer = self.getWidget(self.selected_index-1).pixmap()
-                self.getWidget(5).setPixmap(pixContainer.scaled(self.W-40, self.H-40, Qt.KeepAspectRatio))
+                self.getWidget(5).setPixmap(pixContainer.scaled((self.W/1.33)-40, (self.H/1.28)-40, Qt.KeepAspectRatio))
             self.select(self.selected_index-1)
 
         else:
@@ -76,14 +120,14 @@ class View(QWidget):
                     self.getWidget(i).setPixmap(self.getWidget(i-1).pixmap().scaled(((self.W-40)/5)-20, ((self.W-40)/5)-20, Qt.KeepAspectRatio))
                 self.getWidget(0).setPixmap(QPixmap("data/"+Models.images[self.circularTraverse(1, "left")]).scaled(((self.W-40)/5)-20, ((self.W-40)/5)-20, Qt.KeepAspectRatio))
                 if self.mode == 1:     # If in full screen mode, load previous image
-                    self.getWidget(5).setPixmap(self.getWidget(0).pixmap().scaled(self.W-40, self.H-40, Qt.KeepAspectRatio))
+                    self.getWidget(5).setPixmap(self.getWidget(0).pixmap().scaled((self.W/1.33)-40, (self.H/1.28)-40, Qt.KeepAspectRatio))
                 self.image_index = self.circularTraverse(1, "left")
 
     def shiftRight(self):       # Shifts the select box and scope of the Thumbnail Bar to the right
         if self.selected_index != 4:      # Shift select box till last image
             if self.mode == 1:
                 pixContainer = self.getWidget(self.selected_index+1).pixmap()
-                self.getWidget(5).setPixmap(pixContainer.scaled(self.W-40, self.H-40, Qt.KeepAspectRatio))
+                self.getWidget(5).setPixmap(pixContainer.scaled((self.W/1.33)-40, (self.H/1.28)-40, Qt.KeepAspectRatio))
             self.select(self.selected_index+1)
         else:
             if 5 == 5:      # If there are more than 5 images, shift scope of Thumbnail Bar to rght
@@ -91,7 +135,7 @@ class View(QWidget):
                     self.getWidget(i).setPixmap(self.getWidget(i+1).pixmap().scaled(((self.W-40)/5)-20, ((self.W-40)/5)-20, Qt.KeepAspectRatio))
                 self.getWidget(4).setPixmap(QPixmap("data/"+Models.images[self.circularTraverse(5, "right")]).scaled(((self.W-40)/5)-20, ((self.W-40)/5)-20, Qt.KeepAspectRatio))
                 if self.mode == 1:    # If in full screen mode, load next image
-                    self.getWidget(5).setPixmap(self.getWidget(4).pixmap().scaled(self.W-40, self.H-40, Qt.KeepAspectRatio))
+                    self.getWidget(5).setPixmap(self.getWidget(4).pixmap().scaled((self.W/1.33)-40, (self.H/1.28)-40, Qt.KeepAspectRatio))
                 self.image_index = self.circularTraverse(1, "right")
 
     def shrink(self, index):     # Makes necessary changes to change back to Thumbnail Mode
@@ -100,12 +144,48 @@ class View(QWidget):
         for i in range(0,5):    # Un-hides thumbnail labels
             self.getWidget(i).show()
         self.layout.setContentsMargins(20, 0, 20, 0)
+        self.layout.setAlignment(Qt.AlignVCenter)    # Pushes image to the top of the window
+        self.textBox.hide()
+        self.addButton.hide()
+        self.saveButton.hide()
+        for i in range(0, len(self.getTags())):
+            self.getTags().itemAt(0).widget().setParent(None)
+
+
 
 
 #=========================================== EVENT HANDLERS =========================================
 
+    def loadTags(self):
+        try:
+            f = open("tags.json","r")
+            self.tags = json.load(f)
+            f.close()
+        except:
+            self.tags = {}
+
+    def addTag(self):
+        image_key = str((self.image_index+self.selected_index)%len(Models.images))
+        if image_key not in self.tags:
+            self.tags[image_key] = []
+        self.tags[image_key].append(self.textBox.text())
+        self.textBox.setText("")
+        self.setFocus()
+        self.drawTags()
+
+    def saveTags(self):
+        f = open("tags.json", "w+")
+        try:
+            json.dump(self.tags, f)
+        except:
+            print(type(self.tags))
+        f.close()
+
     def getWidget(self, index):     # Gets stored widget from layout in the passed index
         return self.layout.itemAt(index).widget()
+
+    def getTags(self):
+        return self.layout.itemAt(6).layout()
 
     def indexOfLabel(self, label):      # Provides the index of the clicked label for further operations
         if self.mode == 0:
@@ -130,11 +210,15 @@ class View(QWidget):
         if e.key() == Qt.Key_Right:
             Models.select_sound.play()
             self.shiftRight()
+            if self.mode == 1:
+                self.drawTags()
 
 
         if e.key() == Qt.Key_Left:
             Models.select_sound.play()
             self.shiftLeft()
+            if self.mode == 1:
+                self.drawTags()
 
         elif e.key() == Qt.Key_Period and self.mode == 0 and 5 == 5:    # Moves to the next 5 images only if there are enough images to overflow
             Models.next_set_sound.play()
@@ -172,3 +256,6 @@ class View(QWidget):
                         self.getWidget(0).setPixmap(QPixmap("data/"+Models.images[self.circularTraverse(1, "left")]).scaled(((self.W-40)/5)-20, ((self.W-40)/5)-20, Qt.KeepAspectRatio))
                         self.image_index = self.circularTraverse(1, "left")
                 self.select(2)
+
+        elif e.key() == Qt.Key_Escape:
+            self.setFocus()
